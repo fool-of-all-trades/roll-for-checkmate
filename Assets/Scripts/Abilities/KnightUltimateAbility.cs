64f7b2a338e7af1f2543b44c427694c8aa8efbe8
Marti-code <marti.huza@gmail.com>
2025-07-27 18:42:31 +0200

    setup for Knight's ultimate

--- Assets/Scripts/Abilities/KnightUltimateAbility.cs
+++ Assets/Scripts/Abilities/KnightUltimateAbility.cs
@@ -1,22 +1,31 @@
+using Pieces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// So far 3 ideas:
-//      1. Teleportation Runes – Once per game, the Horse can swap places with an enemy piece instead of attacking. Could backfire? Absolutely.
-//      should they be able to choose the enemy piece? Cuz that could also work as a well save, cuz if some piece got a good check at king, then the Knight could swap with him = saving the king
-//      2. Holy Strike – Can stun an enemy for one turn
-//      3. Divine Smite – One enemy piece can be striked, and it's level turns to 1, and it can't move for one turn (honestly my fav, wonder why...)
-
-public class KnightUltimateAbility : MonoBehaviour
+namespace Abilities
 {
-    void Start()
+    /// <summary>
+    /// Knight's ultimate ability: Divine Smite
+    /// One enemy piece can be striked, and it's level turns to 1,
+    /// and it can't move for one turn
+    /// </summary>
+    public class KnightUltimateAbility : Ability
     {
-        
-    }
+        public KnightUltimateAbility(int levelRequirement) : base(levelRequirement) { }
 
-    void Update()
-    {
-        
+        public override void UseAbility(IGameController controller, Piece owner)
+        {
+            if (usedUltimate)
+            {
+                Debug.Log("Ultimate already used.");
+                return;
+            }
+
+            int ownerRow = owner.Row;
+            int ownerCol = owner.Col;
+
+            Piece target = null;
+        }
     }
 }
